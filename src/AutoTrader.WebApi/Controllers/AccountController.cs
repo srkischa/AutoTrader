6428a65fd81dae8a0f9054f9f3cf96fbf54eeaed
srkischa <s.milovanov@gmail.com>
2016-09-25 22:45:51 +0200

    added user registration and forget password

--- src/AutoTrader.WebApi/Controllers/AccountController.cs
+++ src/AutoTrader.WebApi/Controllers/AccountController.cs
@@ -1,44 +1,151 @@
 ﻿using AutoMapper;
+using AutoTrader.Service;
 using AutoTrader.Service.Identity;
 using AutoTrader.WebApi.Request;
 using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace AutoTrader.WebApi.Controllers
 {
+    [RoutePrefix("api/account")]
     public class AccountController : ApiController
     {
+        private readonly IAuthenticationManager _authenticationManager;
         private readonly IMapper _mapper;
+        private readonly IUserIdentityManagerService _userManagementService;
         private readonly IUserIdentityManagerService _userManager;
+        private readonly ICurrentUserProvider _currentUserProvider;
+        private readonly IConfigurationSettings _configurationSettings;
 
         public AccountController(
             IUserIdentityManagerService userManager,
-            IMapper mapper)
+            IAuthenticationManager authenticationManager,
+            IUserIdentityManagerService userManagementService,
+            IMapper mapper,
+            ICurrentUserProvider currentUserProvider,
+            IConfigurationSettings configurationSettings)
         {
             _userManager = userManager;
+            _authenticationManager = authenticationManager;
+            _userManagementService = userManagementService;
             _mapper = mapper;
+            _currentUserProvider = currentUserProvider;
+            _configurationSettings = configurationSettings;
         }
 
         [HttpPost]
+        [Route("register-user")]
         public async Task<IHttpActionResult> Register(UserRegistrationRequest model)
         {
             var existingUser = await _userManager.FindByNameAsync(model.Email);
-
-            if (existingUser != null) throw new Exception("User with that email alerady exist");
+            if (existingUser != null)
+            {
+                var errorResponse = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("User with that email alerady exist")
+                };
+                throw new HttpResponseException(errorResponse);
+            }
 
             var user = _mapper.Map<ApplicationUser>(model);
 
             var identityResult = await _userManager.CreateAsync(user, model.Password);
 
-            //SendNotificationEmail(model.Email, user);
-
             var errorResult = GetErrorResult(identityResult);
+            if (errorResult != null) return errorResult;
 
-            if (errorResult != null)
+            identityResult = await _userManager.AddToRolesAsync(user.Id, "BasicUser");
+
+            errorResult = GetErrorResult(identityResult);
+            if (errorResult != null) return errorResult;
+
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user.Id);
+
+            var callbackUrl = new Uri($"{_configurationSettings.FrontEndUrl}confirm?userId={user.Id}&code={code}");
+
+            string emailTitle = "Please confirm your account";
+            string emailBody = callbackUrl.AbsoluteUri;
+
+            await _userManager.SendEmailAsync(user.Id, emailTitle, emailBody);
+
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("register-email")]
+        public async Task<IHttpActionResult> ConfirmEmail(int userId, string code)
+        {
+            var identityResult = await _userManager.ConfirmEmailAsync(userId, code);
+            var errorResult = GetErrorResult(identityResult);
+            return errorResult ?? Ok();
+        }
+
+        [Authorize]
+        [Route("change-password")]
+        public async Task<IHttpActionResult> ChangePassword(ChangePasswordRequest model)
+        {
+            var result = await _userManager.ChangePasswordAsync(_currentUserProvider.User.Id, model.OldPassword, model.NewPassword);
+
+            if (!result.Succeeded)
             {
-                return errorResult;
+                return GetErrorResult(result);
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("forget-password")]
+        public async Task<IHttpActionResult> ForgotPassword(ForgotPasswordRequest model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Email);
+            if (user == null || !(await _userManager.IsEmailConfirmedAsync(user.Id)))
+            {
+                var errorResponse = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("Not allowed!")
+                };
+                throw new HttpResponseException(errorResponse);
+            }
+
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user.Id);
+
+            var callbackUrl = new Uri($"{_configurationSettings.FrontEndUrl}reset-password?email={model.Email}&code={code}");
+
+            string emailTitle = "Please reset your password";
+            string emailBody = callbackUrl.AbsoluteUri;
+
+            await _userManager.SendEmailAsync(user.Id, emailTitle, emailBody);
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("reset-password")]
+        public async Task<IHttpActionResult> ResetPassword(RequestPasswordRequest model)
+        {
+            var user = await _userManager.FindByNameAsync(model.Email);
+            if (user == null)
+            {
+                var errorResponse = new HttpResponseMessage(HttpStatusCode.BadRequest)
+                {
+                    Content = new StringContent("Not allowed!")
+                };
+                throw new HttpResponseException(errorResponse);
+            }
+
+            var result = await _userManager.ResetPasswordAsync(user.Id, model.Code, model.Password);
+
+            if (!result.Succeeded)
+            {
+                return GetErrorResult(result);
             }
 
             return Ok();
---
+++ src/AutoTrader.WebApi/Request/ForgotPasswordRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutoTrader.WebApi.Request
+{
+    public class ForgotPasswordRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
